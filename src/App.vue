c9da69ab3d4a2def0344c1784a6f737668d135d7
zidane1105 <hawarimifta@gmail.com>
2025-05-07 23:27:29 +0700

    input newTugas

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <script setup>
+import { ref } from 'vue'
+
+const tasks = ref([])
+const input = ref('')
+
+const addTask = () => {
+  if (input.value){
+
+    tasks.value.push({
+      id: tasks.value.length + 1,
+      name: input.value,
+      completed: false
+    })
+    input.value = ''
+    console.log(tasks.value)
+  }
+}
 </script>
 
 <template>
   <div>
-    
+    <input type="text" v-model="input" @keyup.enter="addTask" />
+    <button @click="addTask">Add Task</button>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
